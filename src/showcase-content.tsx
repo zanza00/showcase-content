9076c28946e228a2b07ce217c00e62ef86f37a27
Simone Picciani <simone.picciani@prima.it>
2025-12-03 23:44:42 +0100

    add target div

--- src/showcase-content.tsx
+++ src/showcase-content.tsx
@@ -3,10 +3,13 @@ import ReactDOMClient from "react-dom/client";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 
+console.log({ React });
+
 const lifecycles = singleSpaReact({
   React,
   ReactDOMClient,
   rootComponent: Root,
+  domElementGetter: () => document.getElementById("main-content")!,
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
